e01f2aafaffd880e2e82cfbca67cd18f662f3efa
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-02 23:50:38 -0600

    [add] get productos

--- api.php
+++ api.php
@@ -2,8 +2,14 @@
 require_once 'vendor/autoload.php';
 $app=new \Slim\Slim();
 
+$db=new mysqli("localhost","root","","prueba1");
 $app->get("/productos",function() use($db,$app){
-
+  $query=$db->query("SELECT * FROM Productos");
+  $productos=array();
+  while ($fila=$query->fetch_assoc()) {
+    $productos[]=$fila;
+  }
+  echo json_encode($productos);
 });
 
 $app->run();
