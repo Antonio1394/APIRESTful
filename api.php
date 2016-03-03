424c343b3c3591997ce3503ae914aad3241d8040
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-03 00:03:40 -0600

    [add] post

--- api.php
+++ api.php
@@ -12,4 +12,14 @@ $app->get("/productos",function() use($db,$app){
   echo json_encode($productos);
 });
 
+$app->post("/productos",function() use($db,$app){
+    $query="INSERT INTO productos VALUES (NULL,"
+      ."{$app->requeest->post("name")},"
+      ."{$app->requeest->post("description")},"
+      ."{$app->requeest->post("price")},"
+      .")";
+    $insert=$db->query($query);
+
+});
+
 $app->run();
