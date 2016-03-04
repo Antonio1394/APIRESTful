ed3550b150c2256ec6f8178e4bd1758074d63d57
Antonio1394 <antoniorivm1394@gmail.com>
2016-03-03 23:56:25 -0600

    [update] post

--- api.php
+++ api.php
@@ -14,9 +14,9 @@ $app->get("/productos",function() use($db,$app){
 
 $app->post("/productos",function() use($db,$app){
     $query="INSERT INTO productos VALUES (NULL,"
-      . "{$app->request->post("name")},"
-      . "{$app->request->post("description")},"
-      . "{$app->request->post("price")},"
+      . "'{$app->request->post("name")}',"
+      . "'{$app->request->post("description")}',"
+      . "'{$app->request->post("price")}'"
       . ")";
     $insert=$db->query($query);
     if ($insert) {
